91bbc1fdb3e0e21abc342166e97dcbb3dc31d958
LuisRojo18 <luisinrf18@gmail.com>
2024-11-14 08:52:42 -0500

    Primer commit

---
+++ ExamT3/Program.cs
@@ -0,0 +1,22 @@
+﻿string opcion;
+
+void MostrarSeleccion()
+{
+
+    Console.WriteLine("a)++++Crear++++\n");
+    Console.WriteLine("b)++++Listar++++\n");
+    Console.WriteLine("+++Modificar Masivo+++\n);");
+    Console.WriteLine("d)++++Fin++++\n");
+    Console.WriteLine("Seleccione una opcion: ");
+}
+bool ContinuarPrograma=true;
+while (ContinuarPrograma)
+{
+MostrarSeleccion();
+    opcion=Console.ReadLine();
+    switch (opcion)
+    {
+        case "a":
+            Console.WriteLine("Ingrese su numero: ");
+    }
+}
